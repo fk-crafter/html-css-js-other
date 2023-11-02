7f1ed1d81899bd5c41a941e28a4c230eea109b7c
Fofo <127132293+fk-crafter@users.noreply.github.com>
2023-11-02 15:38:01 +0100

    Create main.js

---
+++ instagram-login/main.js
@@ -0,0 +1,23 @@
+$(".twitter").hover(function () {
+	$("#social-section").toggleClass("color-twitter")
+});
+
+$(".facebook").hover(function () {
+	$("#social-section").toggleClass("color-facebook")
+});
+
+$(".googleplus").hover(function () {
+	$("#social-section").toggleClass("color-googleplus")
+});
+
+$(".pinterest").hover(function () {
+	$("#social-section").toggleClass("color-pinterest")
+});
+
+$(".dribbble").hover(function () {
+	$("#social-section").toggleClass("color-dribbble")
+});
+
+$(".instagram").hover(function () {
+	$("#social-section").toggleClass("color-instagram")
+});
